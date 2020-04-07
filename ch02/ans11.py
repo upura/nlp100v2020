7c09ecb0f9ca721f2dfdb905236cb93d7ad5cf03
upura <upura0@gmail.com>
2020-04-07 22:07:31 +0900

    fix output file name

--- ch02/ans11.py
+++ ch02/ans11.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 df = pd.read_csv('ch02/popular-names.txt', sep='\t', header=None)
-df.to_csv('ch02/popular-names_11p.txt', sep=' ', index=False, header=None)
+df.to_csv('ch02/ans11.txt', sep=' ', index=False, header=None)
--- ch02/ans11.sh
+++ ch02/ans11.sh
@@ -1,1 +1,1 @@
-sed -e 's/[[:cntrl:]]/ /g' ch02/popular-names.txt >> ch02/popular-names_11s.txt
+sed -e 's/[[:cntrl:]]/ /g' ch02/popular-names.txt >> ch02/ans11.txt
