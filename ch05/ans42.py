e4dc166648fb51915a2c82c56c72486aceff3790
upura <upura0@gmail.com>
2020-04-11 17:07:54 +0900

    ch05

---
+++ ch05/ans42.py
@@ -0,0 +1,61 @@
+class Morph:
+    def __init__(self, dc):
+        self.surface = dc['surface']
+        self.base = dc['base']
+        self.pos = dc['pos']
+        self.pos1 = dc['pos1']
+
+
+class Chunk:
+    def __init__(self, morphs, dst):
+        self.morphs = morphs    # 形態素（Morphオブジェクト）のリスト
+        self.dst = dst          # 係り先文節インデックス番号
+        self.srcs = []          # 係り元文節インデックス番号のリスト
+
+
+def parseCabocha(block):
+    def checkCreateChunk(tmp):
+        if len(tmp) > 0:
+            c = Chunk(tmp, dst)
+            res.append(c)
+            tmp = []
+        return tmp
+
+    res = []
+    tmp = []
+    dst = None
+    for line in block.split('\n'):
+        if line == '':
+            tmp = checkCreateChunk(tmp)
+        elif line[0] == '*':
+            dst = line.split(' ')[2].rstrip('D')
+            tmp = checkCreateChunk(tmp)
+        else:
+            (surface, attr) = line.split('\t')
+            attr = attr.split(',')
+            lineDict = {
+                'surface': surface,
+                'base': attr[6],
+                'pos': attr[0],
+                'pos1': attr[1]
+            }
+            tmp.append(Morph(lineDict))
+
+    for i, r in enumerate(res):
+        res[int(r.dst)].srcs.append(i)
+    return res
+
+
+filename = 'ch05/neko.txt.cabocha'
+with open(filename, mode='rt', encoding='utf-8') as f:
+    blockList = f.read().split('EOS\n')
+blockList = list(filter(lambda x: x != '', blockList))
+blockList = [parseCabocha(block) for block in blockList]
+
+for b in blockList:
+    for m in b:
+        if int(m.dst) > -1:
+            print(*[
+                ''.join([mo.surface for mo in m.morphs]),
+                ''.join([mo.surface for mo in b[int(m.dst)].morphs])
+            ], sep='\t')
---
+++ ch05/ans43.py
@@ -0,0 +1,63 @@
+class Morph:
+    def __init__(self, dc):
+        self.surface = dc['surface']
+        self.base = dc['base']
+        self.pos = dc['pos']
+        self.pos1 = dc['pos1']
+
+
+class Chunk:
+    def __init__(self, morphs, dst):
+        self.morphs = morphs    # 形態素（Morphオブジェクト）のリスト
+        self.dst = dst          # 係り先文節インデックス番号
+        self.srcs = []          # 係り元文節インデックス番号のリスト
+
+
+def parseCabocha(block):
+    def checkCreateChunk(tmp):
+        if len(tmp) > 0:
+            c = Chunk(tmp, dst)
+            res.append(c)
+            tmp = []
+        return tmp
+
+    res = []
+    tmp = []
+    dst = None
+    for line in block.split('\n'):
+        if line == '':
+            tmp = checkCreateChunk(tmp)
+        elif line[0] == '*':
+            dst = line.split(' ')[2].rstrip('D')
+            tmp = checkCreateChunk(tmp)
+        else:
+            (surface, attr) = line.split('\t')
+            attr = attr.split(',')
+            lineDict = {
+                'surface': surface,
+                'base': attr[6],
+                'pos': attr[0],
+                'pos1': attr[1]
+            }
+            tmp.append(Morph(lineDict))
+
+    for i, r in enumerate(res):
+        res[int(r.dst)].srcs.append(i)
+    return res
+
+
+filename = 'ch05/neko.txt.cabocha'
+with open(filename, mode='rt', encoding='utf-8') as f:
+    blockList = f.read().split('EOS\n')
+blockList = list(filter(lambda x: x != '', blockList))
+blockList = [parseCabocha(block) for block in blockList]
+
+for b in blockList:
+    for m in b:
+        if int(m.dst) > -1:
+            preText = ''.join([mo.surface for mo in m.morphs])
+            prePos = [mo.pos for mo in m.morphs]
+            postText = ''.join([mo.surface for mo in b[int(m.dst)].morphs])
+            postPos = [mo.pos for mo in b[int(m.dst)].morphs]
+            if '名詞' in prePos and '動詞' in postPos:
+                print(*[preText, postText], sep='\t')
