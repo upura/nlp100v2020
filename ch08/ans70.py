a94457427006edbdb922b85ccb501de8c8c67931
upura <upura0@gmail.com>
2020-04-12 19:09:47 +0900

    ch08

---
+++ ch08/ans70.py
@@ -0,0 +1,55 @@
+import joblib
+import numpy as np
+import pandas as pd
+from gensim.models import KeyedVectors
+from tqdm import tqdm
+
+
+def culcSwem(row):
+    global model
+    swem = []
+    for w in row['TITLE'].split():
+        if w in model.vocab:
+            swem.append(model[w])
+    swem = np.array(swem)
+    if len(swem) > 0:
+        swem = np.mean(swem, axis=0)
+    else:
+        swem = np.zeros(300,)
+    return swem
+
+
+X_train = pd.read_table('ch06/train.txt', header=None)
+X_valid = pd.read_table('ch06/valid.txt', header=None)
+X_test = pd.read_table('ch06/test.txt', header=None)
+use_cols = ['TITLE', 'CATEGORY']
+n_train = len(X_train)
+n_valid = len(X_valid)
+n_test = len(X_test)
+X_train.columns = use_cols
+X_valid.columns = use_cols
+X_test.columns = use_cols
+
+data = pd.concat([X_train, X_valid, X_test]).reset_index(drop=True)
+
+tqdm.pandas()
+model = KeyedVectors.load_word2vec_format('ch07/GoogleNews-vectors-negative300.bin', binary=True)
+swemVec = data.progress_apply(culcSwem, axis=1)
+
+X_train = np.array(swemVec[:n_train])
+X_valid = np.array(swemVec[n_train:n_train + n_valid])
+X_test = np.array(swemVec[n_train + n_valid:])
+joblib.dump(X_train, 'ch08/X_train.joblib')
+joblib.dump(X_valid, 'ch08/X_valid.joblib')
+joblib.dump(X_test, 'ch08/X_test.joblib')
+
+y_data = data['CATEGORY'].map({'b': 0, 'e': 1, 't': 2, 'm': 3})
+y_data = np.identity(4)[y_data]
+
+y_train = y_data[:n_train]
+y_valid = y_data[n_train:n_train + n_valid]
+y_test = y_data[n_train + n_valid:]
+
+joblib.dump(y_train, 'ch08/y_train.joblib')
+joblib.dump(y_valid, 'ch08/y_valid.joblib')
+joblib.dump(y_test, 'ch08/y_test.joblib')
