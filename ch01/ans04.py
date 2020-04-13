bf4d0413be57742941a8953ba97b404b6ea1e83b
upura <upura0@gmail.com>
2020-04-14 03:20:05 +0900

    refactoring

--- ch01/ans04.py
+++ ch01/ans04.py
@@ -1,11 +1,11 @@
 def extWord(i, word):
     if i in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
-        return (word[0], i + 1)
+        return (word[0], i)
     else:
-        return (word[:2], i + 1)
+        return (word[:2], i)
 
 
 rawText = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
 text = rawText.replace('.', '').replace(',', '')
-ans = [extWord(i, w) for i, w in enumerate(text.split())]
+ans = [extWord(i, w) for i, w in enumerate(text.split(), 1)]
 print(dict(ans))
