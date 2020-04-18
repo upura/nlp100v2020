a26964b65eddaa60d6e76eeac0bcdc479ab996cd
upura <upura0@gmail.com>
2020-04-19 02:18:05 +0900

    ch10

--- ch10/ans92.py
+++ ch10/ans92.py
@@ -1,24 +1,8 @@
-import math
-import time
 import random
-from tqdm import tqdm
-import pandas as pd
 from torchtext import data
 import torch
-from torch import nn, optim
+from torch import nn
 import torch.nn.functional as F
-
-
-def load_concat_save(category, is_small=False):
-    train_en = pd.read_table(f'ch10/kftt-data-1.0/data/tok/kyoto-{category}.en', header=None)
-    train_ja = pd.read_table(f'ch10/kftt-data-1.0/data/tok/kyoto-{category}.ja', header=None, names=[0, 1])
-    train_ja.loc[~train_ja[1].isnull(), 0] = train_ja.loc[~train_ja[1].isnull()][0] + ' ' + train_ja.loc[~train_ja[1].isnull()][1]
-    train = pd.concat([train_en, train_ja[0]], axis=1)
-    if is_small:
-        train = train.loc[:1000]
-        train.to_csv(f'ch10/kftt-data-1.0/data/tok/kyoto-{category}_small.txt', sep='\t', index=False, header=None)
-    else:
-        train.to_csv(f'ch10/kftt-data-1.0/data/tok/kyoto-{category}.txt', sep='\t', index=False, header=None)
 
 
 class Encoder(nn.Module):
@@ -173,6 +157,7 @@ class Seq2Seq(nn.Module):
         output = trg[0, :]
 
         for t in range(1, max_len):
+
             output, hidden = self.decoder(
                 output,
                 hidden,
@@ -198,44 +183,9 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def train(model, iterator, optimizer, criterion, clip):
-    model.train()
-    epoch_loss = 0
-
-    for _, batch in enumerate(iterator):
-
-        ja = batch.JA
-        en = batch.EN
-
-        ja = ja.to(device)
-        en = en.to(device)
-
-        optimizer.zero_grad()
-
-        output = model(ja, en)
-
-        output = output[1:].view(-1, output.shape[-1])
-        en = en[1:].view(-1)
-
-        output = output.to(device)
-        en = en.to(device)
-
-        loss = criterion(output, en)
-        loss.backward()
-
-        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
-
-        optimizer.step()
-
-        epoch_loss += loss.item()
-
-    return epoch_loss / len(iterator)
-
-
-def evaluate(model, iterator, criterion):
+def predict(model, iterator):
     model.eval()
-    epoch_loss = 0
-
+    y_preds = []
     with torch.no_grad():
 
         for _, batch in enumerate(iterator):
@@ -247,18 +197,18 @@ def evaluate(model, iterator, criterion):
             en = en.to(device)
 
             output = model(ja, en, 0)     # turn off teacher forcing
-
-            output = output[1:].view(-1, output.shape[-1])
-            en = en[1:].view(-1)
-
             output = output.to(device)
-            en = en.to(device)
+            y_pred = torch.max(output, 2)[1]
+            y_preds.append(y_pred)
 
-            loss = criterion(output, en)
+    return y_preds
 
-            epoch_loss += loss.item()
 
-    return epoch_loss / len(iterator)
+def decode_text(en):
+    en = list(en)
+    en = [EN.vocab.itos[e] for e in en]
+    text = ' '.join(en[1:])
+    return text
 
 
 def epoch_time(start_time, end_time):
@@ -268,30 +218,72 @@ def epoch_time(start_time, end_time):
     return elapsed_mins, elapsed_secs
 
 
-prepare_dataset = False
-use_small = True
-prepare_list = ['train', 'dev', 'test']
-if prepare_dataset:
-    for pl in prepare_list:
-        load_concat_save(pl, use_small)
+def load_model(JA, EN, device):
+    INPUT_DIM = len(JA.vocab)     # 入力データの単語数
+    OUTPUT_DIM = len(EN.vocab)    # 出力データの単語数
+
+    ENC_EMB_DIM = 32               # Encoder用のembeddingの次元数
+    DEC_EMB_DIM = 32               # Decoder用のembeddingの次元数
+    ENC_HID_DIM = 64               # Encoder用の隠れ層の次元数
+    DEC_HID_DIM = 64               # Decoder用の隠れ層の次元数
+    ATTN_DIM = 8                   # Attentionの隠れ層の次元数
+    ENC_DROPOUT = 0.5              # Encoder用のDropout確率
+    DEC_DROPOUT = 0.5              # Decoder用のDropout確率
+
+    enc = Encoder(
+        input_dim=INPUT_DIM,
+        emb_dim=ENC_EMB_DIM,
+        enc_hid_dim=ENC_HID_DIM,
+        dec_hid_dim=DEC_HID_DIM,
+        dropout=ENC_DROPOUT
+    )
+
+    attn = Attention(
+        enc_hid_dim=ENC_HID_DIM,
+        dec_hid_dim=DEC_HID_DIM,
+        attn_dim=ATTN_DIM
+    )
+
+    dec = Decoder(
+        output_dim=OUTPUT_DIM,
+        emb_dim=DEC_EMB_DIM,
+        enc_hid_dim=ENC_HID_DIM,
+        dec_hid_dim=DEC_HID_DIM,
+        dropout=DEC_DROPOUT,
+        attention=attn
+    )
+
+    model = Seq2Seq(
+        encoder=enc,
+        decoder=dec
+    )
+
+    model.apply(init_weights)
+    model = model.to(device)
+    model.load_state_dict(torch.load('ch10/model.pth', map_location=torch.device('cpu')))
+    model.eval()
+    return model
+
 
 EN = data.Field(sequential=True, init_token='<sos>', eos_token='<eos>', lower=True)
 JA = data.Field(sequential=True, init_token='<sos>', eos_token='<eos>', lower=True)
 
+ja_text = '日本 の 水墨 画 を 一変 さ せ た 。'
+en_text = ' '.join(['<sos>'] * len(ja_text.split()))
+test_text = f'{en_text}\t{ja_text}'
+with open('ch10/kftt-data-1.0/data/tok/tmp.txt', mode='w') as f:
+    f.write(test_text)
+
+use_small = True
 if use_small:
     train_path = 'kyoto-train_small.txt'
-    valid_path = 'kyoto-dev_small.txt'
-    test_path = 'kyoto-test_small.txt'
 else:
     train_path = 'kyoto-train.txt'
-    valid_path = 'kyoto-dev.txt'
-    test_path = 'kyoto-test.txt'
 
-train_data, valid_data, test_data = data.TabularDataset.splits(
+train_data, test_data = data.TabularDataset.splits(
     path='ch10/kftt-data-1.0/data/tok',
     train=train_path,
-    validation=valid_path,
-    test=test_path,
+    test='tmp.txt',
     format='tsv',
     fields=[('EN', EN), ('JA', JA)])
 
@@ -299,92 +291,10 @@ EN.build_vocab(train_data, min_freq=2)
 JA.build_vocab(train_data, min_freq=2)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
-    (train_data, valid_data, test_data), device=device, batch_size=128, sort=False)
+train_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, test_data), device=device, batch_size=128, sort=False)
 
-batch = next(iter(train_iterator))
-print(batch.EN)
-print(batch.JA)
+model = load_model(JA, EN, device)
+y_preds = predict(model, test_iterator)
 
-INPUT_DIM = len(JA.vocab)     # 入力データの単語数
-OUTPUT_DIM = len(EN.vocab)    # 出力データの単語数
-
-ENC_EMB_DIM = 32               # Encoder用のembeddingの次元数
-DEC_EMB_DIM = 32               # Decoder用のembeddingの次元数
-ENC_HID_DIM = 64               # Encoder用の隠れ層の次元数
-DEC_HID_DIM = 64               # Decoder用の隠れ層の次元数
-ATTN_DIM = 8                   # Attentionの隠れ層の次元数
-ENC_DROPOUT = 0.5              # Encoder用のDropout確率
-DEC_DROPOUT = 0.5              # Decoder用のDropout確率
-
-enc = Encoder(
-    input_dim=INPUT_DIM,
-    emb_dim=ENC_EMB_DIM,
-    enc_hid_dim=ENC_HID_DIM,
-    dec_hid_dim=DEC_HID_DIM,
-    dropout=ENC_DROPOUT
-)
-
-attn = Attention(
-    enc_hid_dim=ENC_HID_DIM,
-    dec_hid_dim=DEC_HID_DIM,
-    attn_dim=ATTN_DIM
-)
-
-dec = Decoder(
-    output_dim=OUTPUT_DIM,
-    emb_dim=DEC_EMB_DIM,
-    enc_hid_dim=ENC_HID_DIM,
-    dec_hid_dim=DEC_HID_DIM,
-    dropout=DEC_DROPOUT,
-    attention=attn
-)
-
-model = Seq2Seq(
-    encoder=enc,
-    decoder=dec
-)
-
-model.apply(init_weights)
-model = model.to(device)
-optimizer = optim.Adam(model.parameters())
-print(model)
-
-N_EPOCHS = 30
-CLIP = 1
-PAD_IDX = EN.vocab.stoi['<pad>']
-
-criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
-best_valid_loss = float('inf')
-
-train_loss_list = []
-valid_loss_list = []
-for epoch in tqdm(range(N_EPOCHS)):
-
-    start_time = time.time()
-
-    train_loss = train(model,
-                       train_iterator,
-                       optimizer,
-                       criterion,
-                       CLIP)
-
-    valid_loss = evaluate(model,
-                          valid_iterator,
-                          criterion)
-
-    end_time = time.time()
-
-    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-
-    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
-    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
-    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
-
-    train_loss_list.append(train_loss)
-    valid_loss_list.append(valid_loss)
-
-test_loss = evaluate(model, test_iterator, criterion)
-
-print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
-torch.save(model.state_dict(), 'ch10/model.pth')
+print(decode_text(torch.transpose(y_preds[0], 0, 1)[0]))
