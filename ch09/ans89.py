69d5d49a555eff9b7e6d32085134f0dc4603445d
upura <upura0@gmail.com>
2023-11-05 23:35:41 +0900

    Update ch09

--- ch09/ans89.py
+++ ch09/ans89.py
@@ -1,55 +1,77 @@
-import torch
-from torch import optim
-from torchtext import data
-from tqdm import tqdm
-from transformers import BertForSequenceClassification
+import os
+
+import datasets
+import evaluate
+import numpy as np
+import pandas as pd
+from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
+                          DataCollatorWithPadding, Trainer, TrainingArguments)
 
 
-def eval_net(model, data_loader, device='cpu'):
-    model.eval()
-    ys = []
-    ypreds = []
-    for x, y, _ in data_loader:
-        with torch.no_grad():
-            loss, logit = model(input_ids=x, labels=y)
-            _, y_pred = torch.max(logit, 1)
-            ys.append(y)
-            ypreds.append(y_pred)
-    ys = torch.cat(ys)
-    ypreds = torch.cat(ypreds)
-    print(f'test acc: {(ys == ypreds).sum().item() / len(ys)}')
-    return
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True, max_length=512)
 
 
-TEXT = data.Field(sequential=True, lower=True, batch_first=True)
-LABELS = data.Field(sequential=False, batch_first=True, use_vocab=False)
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
-train, val, test = data.TabularDataset.splits(
-    path='ch06', train='train2.txt',
-    validation='valid2.txt', test='test2.txt', format='tsv',
-    fields=[('TEXT', TEXT), ('LABEL', LABELS)])
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-train_iter, val_iter, test_iter = data.Iterator.splits(
-    (train, val, test), batch_sizes=(64, 64, 64), device=device, repeat=False, sort=False)
+if __name__ == "__main__":
 
-TEXT.build_vocab(train, min_freq=2)
-LABELS.build_vocab(train)
+    metric = evaluate.load("accuracy")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=4)
-model = model.to(device)
+    df = pd.read_table(f'ch06/train.txt', header=None)
+    df.columns = ["text", "label"]
+    valid = pd.read_table(f'ch06/valid.txt', header=None)
+    valid.columns = ["text", "label"]
+    test = pd.read_table(f'ch06/test.txt', header=None)
+    test.columns = ["text", "label"]
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+    train_dataset = datasets.Dataset.from_pandas(df[["text", "label"]])
+    train_tokenized = train_dataset.map(preprocess_function, batched=True)
+    val_dataset = datasets.Dataset.from_pandas(valid[["text", "label"]])
+    val_tokenized = val_dataset.map(preprocess_function, batched=True)
+    test_dataset = datasets.Dataset.from_pandas(test[["text"]])
+    test_tokenized = test_dataset.map(preprocess_function, batched=True)
 
-for epoch in tqdm(range(10)):
-    losses = []
-    model.train()
-    for batch in train_iter:
-        x, y = batch.TEXT, batch.LABEL
-        loss, logit = model(input_ids=x, labels=y)
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-        _, y_pred_train = torch.max(logit, 1)
-    eval_net(model, test_iter, device)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "bert-base-uncased", num_labels=4
+    )
+
+    training_args = TrainingArguments(
+        output_dir=f"./results",
+        learning_rate=4e-5,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=64,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        evaluation_strategy="steps",
+        eval_steps=250,
+        load_best_model_at_end=True,
+        save_steps=1000,
+        gradient_accumulation_steps=3,
+        save_total_limit=3,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_tokenized,
+        eval_dataset=val_tokenized,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+    oof_results = trainer.predict(test_dataset=val_tokenized)
+    np.save(f"oof_prediction", oof_results.predictions)
+
+    results = trainer.predict(test_dataset=test_tokenized)
+    np.save(f"test_prediction", results.predictions)
